60e5e32975fe0b78dae0e6a7229790d0f9fb1d76
Ankit Ram Nag <ankit.nag@conzentus.com>
2025-06-13 14:41:10 +0530

    Auto-generated commit

--- src/app/routeguard/auth.admin.guard.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateChildFn, Router } from '@angular/router';
-
-export const authAdminGuard: CanActivateChildFn = (childRoute, state) => {
-  const router = inject(Router);
-  const isLoggedIn = !!localStorage.getItem('token');
-
-  return isLoggedIn ? true : router.parseUrl('/');
-};
