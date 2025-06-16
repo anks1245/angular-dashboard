9362984df5bfca8a5d4e39a950e01a92a2f0168c
Ankit Ram Nag <ankit.nag@conzentus.com>
2025-06-16 11:35:11 +0530

    fixed sidebar

--- src/app/protected/dashboard/analytics/analytics.component.ts
+++ src/app/protected/dashboard/analytics/analytics.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Sample } from '../../../data/sample';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-analytics',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
 export class AnalyticsComponent {
+  data: string = ""
+  datalist = Sample;
 
+  handleSubmit(){
+    console.log(this.data);
+    
+    Sample.push(this.data)
+  }
 }
--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,16 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SidebarModule } from 'primeng/sidebar';
 import { AppHeaderComponent } from '../../components/app-header/app-header.component';
 import { AppSidebarComponent } from "../../components/app-sidebar/app-sidebar.component";
+import { SidebarService } from '../../shared/sidebar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterModule, SidebarModule, AppHeaderComponent, AppSidebarComponent],
+  imports: [RouterModule, SidebarModule, AppHeaderComponent, AppSidebarComponent, CommonModule],
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  sidebarService = inject(SidebarService)
+  visible = !(window.innerWidth <= 768);
+  ngOnInit(): void {
+    this.sidebarService.sidebarVisible$.subscribe(v=>{
+      console.log(v);
+      this.visible = v;
+    })
+  }
 
+  
 }
